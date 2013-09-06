2cd92b9d301aec18194f39e4da40b163ca33723e
Frank Reding <frank.reding@gmail.com>
2013-09-06 14:49:01 +0100

    Top nav, nano templates, smooth scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,9 @@
-var options = {
-	apiURL: "http://mottokrosh.local/IndirectPayments/wordpress/api/",
+var apiBase = "http://mottokrosh.local/IndirectPayments/wordpress/api/",
+	options = {
+	apiEndpoints: {
+		posts: apiBase + "get_posts/",
+		pages: apiBase + "get_page_index/"
+	},
 	debug: true
 };
 
@@ -11,9 +15,18 @@ window.log = function () {
 	}
 };
 
-function get_latest_content(successCallback) {
+// Nano Templates (Tomasz Mazur, Jacek Becela, https://github.com/trix/nano)
+function nano(template, data) {
+	return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
+		var keys = key.split("."), v = data[keys.shift()];
+		for (var i = 0, l = keys.length; i < l; i++) v = v[keys[i]];
+		return (typeof v !== "undefined" && v !== null) ? v : "";
+	});
+}
+
+function get_latest_content(type, successCallback) {
 	$.ajax({
-		url: options.apiURL + "get_posts/",
+		url: options.apiEndpoints[type],
 		cache: false,
 		success: successCallback,
 		error: function (xhr, textStatus, thrownError) {
@@ -22,9 +35,13 @@ function get_latest_content(successCallback) {
 	});
 }
 
-function display_content(posts) {
+function display_posts(posts) {
+	if ( !posts ) {
+		return;
+	}
+
 	// reset contents
-	$("#content, #sideNav ul").html("");
+	$("#container, #sideNav ul").html("");
 
 	// parse if necessary
 	if ( !$.isArray(posts) ) {
@@ -33,8 +50,27 @@ function display_content(posts) {
 
 	// content
 	$.each(posts, function (i, post) {
-		$("#content").append("<h2>" + post.title + "</h2>\n" + post.content);
-		$("#sideNav ul").append('<li><a href="#' + post.slug + '">' + post.title + '</a></li>');
+		$("#container").append(nano('<article><h2 id="{slug}">{title}</h2>{content}</article>', post));
+		$("#sideNav ul").append(nano('<li><a href="#{slug}">{title}</a></li>', post));
+	});
+}
+
+function display_pages(pages) {
+	if ( !pages ) {
+		return;
+	}
+
+	// reset contents
+	$("#topNav ul").html("");
+
+	// parse if necessary
+	if ( !$.isArray(pages) ) {
+		pages = JSON.parse(pages);
+	}
+
+	// content
+	$.each(pages, function (i, page) {
+		$("#topNav ul").append(nano('<li><a href="#{slug}">{title}</a></li>', page));
 	});
 }
 
@@ -43,16 +79,62 @@ var snapper = new Snap({
 	element: document.getElementById('content')
 });
 
+// smooth anchor scrolling
+/*$("body").on("click", "a[href*='#']:not([href='#'])", function (evt) {
+	if ( location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+		var $target = $(this.hash), target = this.hash;
+		log($target, target);
+
+		$target = $target.length ? $target : $('[name=' + this.hash.slice(1) +']');
+
+		if ($target.length) {
+			evt.preventDefault();
+			var offset = "-" + $target.offset().top + "px";
+			log(offset);
+			$("#contentWrapper").css({ "transition": "all 1s ease" });
+			$("#contentWrapper").css({ "margin-top": offset });
+			$("#contentWrapper").on("webkitTransitionEnd transitionend msTransitionEnd oTransitionEnd", function () {
+				//debugger;
+				$("#contentWrapper").css("transition", "none");
+				$("#contentWrapper").css({ "margin-top": "0" });
+				$("#content").scrollTop($target.offset().top);
+			});
+		}
+	}
+});*/
+
+$("body").on("click", "a", function (e) {
+	e.preventDefault();
+	var offset = $(this.hash).offset().top + $("#content").scrollTop(),
+		velocity = 600.0,  // pixels/sec
+		duration = Math.abs(parseFloat($(this.hash).offset().top)/velocity);
+	log(this.hash, offset, duration);
+	//$("#content").scrollTop(offset);
+	$("#content").animate({ scrollTop: offset }, duration*1000);
+});
+
 // display locally stored content
-display_content(localStorage["posts"]);
+display_posts(localStorage["posts"]);
+display_pages(localStorage["pages"]);
 
 // attempt to get latest content from remote
-get_latest_content(function (data) {
+get_latest_content("posts", function (data) {
 	log(data);
 
 	// display it
-	display_content(data.posts);
+	display_posts(data.posts);
 
 	// store it
 	localStorage["posts"] = JSON.stringify(data.posts);
+
+	// get pages too
+	get_latest_content("pages", function (data) {
+		log(data);
+
+		// display it
+		display_pages(data.pages);
+
+		// store it
+		localStorage["pages"] = JSON.stringify(data.pages);
+	});
 });
